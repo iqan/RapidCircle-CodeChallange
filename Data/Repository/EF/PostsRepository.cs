bc0bc45f042030a2dcc89d7fd26de3edada01214
iqans <iqan.shaikh@gmail.com>
2017-08-20 18:11:05 +0530

    small change and fix

--- Data/Repository/EF/PostsRepository.cs
+++ Data/Repository/EF/PostsRepository.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using Models;
+using System.Linq;
 
 namespace Data.Repository.EF
 {
@@ -14,17 +15,12 @@ namespace Data.Repository.EF
             _context = context;
         }
 
-        public int AddPost(Models.Posts post)
+        public int AddPost(Posts post)
         {
             throw new NotImplementedException();
         }
 
         public void DeletePost(int postId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Dispose()
         {
             throw new NotImplementedException();
         }
@@ -36,12 +32,37 @@ namespace Data.Repository.EF
 
         public IEnumerable<Posts> GetPostsByUserId(string userId)
         {
-            throw new NotImplementedException();
+            return _context.Posts.Where(p => p.UserId.Equals(userId));
         }
 
-        public int UpdatePost(Models.Posts post)
+        public int UpdatePost(Posts post)
         {
             throw new NotImplementedException();
         }
+
+
+        #region Dispose Methods
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion    }
     }
 }
