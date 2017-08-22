04b1189b4b4dafc755d10bdb32866d95437534ff
iqans <iqan.shaikh@gmail.com>
2017-08-22 09:43:51 +0530

    friends add and get methods added in api

---
+++ API/Controllers/FriendsController.cs
@@ -0,0 +1,42 @@
+﻿using Data.Repository;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace API.Controllers
+{
+    [Authorize]
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class FriendsController : ApiController
+    {
+        private readonly IFriendsRepository _repository;
+
+        public FriendsController(IFriendsRepository repository)
+        {
+            _repository = repository;
+        }
+
+        // GET: api/Friends/5
+        public IEnumerable<Friends> Get(string id)
+        {
+            return _repository.GetFriendsById(id);
+        }
+
+        // POST: api/Friends
+        public IHttpActionResult Post([FromBody]Friends friend)
+        {
+            var result = _repository.AddFriend(friend);
+            return CreatedAtRoute("DefaultApi", new { controller = "friends", id = friend.Id }, friend);
+        }
+
+        // DELETE: api/Friends/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- Data/Repository/EF/FriendsRepository.cs
+++ Data/Repository/EF/FriendsRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Models;
 using Data.DataContext;
+using System.Linq;
 
 namespace Data.Repository.EF
 {
@@ -15,32 +16,62 @@ namespace Data.Repository.EF
         }
         public int AddFriend(Friends Friend)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _context.Friends.Add(Friend);
+                return _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                // Log error
+            }
+            return 0;
         }
 
         public void DeleteFriend(int userId, int FriendId)
         {
-            throw new NotImplementedException();
-        }
-
-        public void Dispose()
-        {
-            throw new NotImplementedException();
+            // TODO implement
         }
 
         public IEnumerable<Friends> GetAllFriends()
         {
-            throw new NotImplementedException();
+            // TODO implement
+            return null;
         }
 
         public IEnumerable<Friends> GetFriendsById(string userId)
         {
-            throw new NotImplementedException();
+            return _context.Friends.Where(f => f.UserId == userId);
         }
 
         public int UpdateFriend(Friends Friend)
         {
-            throw new NotImplementedException();
+            // TODO implement
+            return 1;
         }
+
+        #region Dispose Methods
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion    }
     }
 }
